a6c302389aedab893e77ac9de8119ebb00c6b0f6
engle212 <engle.212@buckeyemail.osu.edu>
2026-07-12 10:46:26 -0400

    Fix border loading issue and image-border issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Routes, Route } from 'react-router';
 import Home from './pages/Home';
 import './App.css'
-import { attachWindowEventListeners } from './assets/ts/util.ts';
-
 
 function App() {
-  attachWindowEventListeners();
   return (
     <Routes>
       <Route index element={<Home />} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import { useEffect} from 'react'
 import ColumnLayout from '../components/ColumnLayout'
 import NavBar from '../components/NavBar'
+import { attachWindowEventListeners } from '../assets/ts/util.ts';
 
 function Home() {
+    useEffect(() => {
+    const timer = setTimeout(() => {
+      attachWindowEventListeners();
+    }, 0);
+  });
   return (
     <>
       <NavBar/>
